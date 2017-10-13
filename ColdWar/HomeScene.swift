c7bd642e5d79001e30c8ab8baece6d51696f231f
svaswani <svaswani80@gmail.com>
2017-10-13 01:53:58 -0400

    Added more content to instructions

--- ColdWar/HomeScene.swift
+++ ColdWar/HomeScene.swift
@@ -28,27 +28,36 @@ class HomeScene: SKScene {
         let label = SKLabelNode(fontNamed: GameData.font.mainFont)
         let label2 = SKLabelNode(fontNamed: GameData.font.mainFont)
         let label3 = SKLabelNode(fontNamed: GameData.font.mainFont)
+        let label5 = SKLabelNode(fontNamed: GameData.font.mainFont)
+
         
         label.text = "Cold War"
         label2.text = "This is a two player game. \n Drag to move with one finger. \n Tap with another finger to shoot."
         label3.text = "Avoid enemy projectiles. \n Collect power ups. \n The first to lose 5 lives is out."
+        label5.text = "Stay on your side of the screen. \n 3 Finger double tap to pause."
         
         label.fontSize = 200
         label2.fontSize = 18
         label3.fontSize = 20
+        label5.fontSize = 20
+
 
         label.position = CGPoint(x:size.width/2, y:size.height/2 + 100)
         label2.position = CGPoint(x:size.width/2, y:size.height/2 - 120)
         label3.position = CGPoint(x:size.width/2, y:size.height/2 - 160)
+        label5.position = CGPoint(x:size.width/2, y:size.height/2 - 200)
+
         
         label.zPosition = 1
         label2.zPosition = 1
         label3.zPosition = 1
-        
+        label5.zPosition = 1
+
         addChild(label)
         addChild(label2)
         addChild(label3)
-        
+        addChild(label5)
+
         
         let label4 = SKLabelNode(fontNamed: GameData.font.mainFont)
         label4.text = "Tap to continue"
