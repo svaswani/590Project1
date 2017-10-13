7dec989b5587fc75630147fe197662047dd6646c
svaswani <svaswani80@gmail.com>
2017-10-13 18:12:35 -0400

    Updated game over font color

--- ColdWar/GameOverScene.swift
+++ ColdWar/GameOverScene.swift
@@ -42,7 +42,7 @@ class GameOverScene: SKScene {
         
         let label4 = SKLabelNode(fontNamed: GameData.font.mainFont)
         label4.text = "Tap to play again"
-        label4.fontColor = UIColor.red
+        label4.fontColor = UIColor.purple
         label4.fontSize = 70
         label4.position = CGPoint(x:size.width/2, y:size.height/2 - 300)
         addChild(label4)
